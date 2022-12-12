883c50efefc106ad52b2cda7296363cf2881c5a4
vikasborage <105040606+vikasborage@users.noreply.github.com>
2022-12-12 18:15:56 +0530

    delete unused code files

--- lib/screens/store/binding/store_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:customer_app/screens/store/controller/store_controller.dart';
-import 'package:get/get.dart';
-
-class StoreBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(StoreController());
-  }
-}
